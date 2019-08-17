7c93fb997aac95644eb608ea3e252200070b6ea7
Ezekiel Olugbami <ezekiel.olugbami@multiskills-ng.com>
2019-08-17 20:19:35 +0100

    Cleaned up the todo functionalities

--- todos.js
+++ todos.js
@@ -4,26 +4,39 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit"){
     if(input === "list"){
-        console.log("*****************");
-        todos.forEach(function(todo, i){
-            console.log(i + ": " + todo);
-        });
-        console.log("*****************");
+        listTodos();
     } else if(input === "new"){
-        //ask for new todo
-        var newTodo = prompt("Enter new todo");
-        //add to todo array
-        todos.push(newTodo);
-        console.log("Added Todo")
+        addTodo();
     } else if(input === "delete"){
-        //ask for indecx of todo to be deleted
-        var index = prompt("Enter index of todo to delete");
-        //delete the todo
-        todos.splice(index, 1);
-        console.log("Deleted Todo");
+        deleteTodo();
     }
     // ask again for new input
     input = prompt("What would you like to do?");
 }
 
 console.log("You quit the App");
+
+
+function listTodos() {
+    console.log("*****************");
+    todos.forEach(function(todo, i){
+        console.log(i + ": " + todo);
+    });
+    console.log("*****************");
+}
+
+function addTodo() {
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to todo array
+    todos.push(newTodo);
+    console.log("Added Todo")
+}
+
+function deleteTodo() {
+    //ask for indecx of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    //delete the todo
+    todos.splice(index, 1);
+    console.log("Deleted Todo");
+}
